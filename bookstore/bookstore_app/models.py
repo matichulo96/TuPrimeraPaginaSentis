c2c955d20afacda297b9a90517a5f65e6540b158
Matias Sentis <matiassentis@gmail.com>
2023-09-30 01:13:46 -0300

    Se crean los modelos de clases

--- bookstore/bookstore_app/models.py
+++ bookstore/bookstore_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=30)
+    country = models.CharField(max_length=15)
+    
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    rate = models.CharField(max_length=10)     
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=30)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    year = models.IntegerField()
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
